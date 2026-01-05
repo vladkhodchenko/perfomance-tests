5dd71d44adb3a4bb8cf887ddededc4f0ed2160c5
hodchenko <sidyukvlad@gmail.com>
2026-01-06 00:03:08 +0300

    15.1 Настройки нагрузочных тестов с Pydantic, Практика: Применение настроек в проекте

--- clients/grpc/gateway/client.py
+++ clients/grpc/gateway/client.py
@@ -1,6 +1,7 @@
 from grpc import Channel, insecure_channel, intercept_channel
 from clients.grpc.interceptrors.locust_interceptors import LocustInterceptor
 from locust.env import Environment
+from config import settings
 
 
 def build_gateway_grpc_client() -> Channel:
@@ -9,7 +10,7 @@ def build_gateway_grpc_client() -> Channel:
 
     :return: gRPC-канал (Channel), настроенный на адрес localhost:9003.
     """
-    return insecure_channel("localhost:9003")
+    return insecure_channel(settings.gateway_grpc_client.client_url)
 
 
 def build_gateway_locust_grpc_client(environment: Environment) -> Channel:
@@ -23,5 +24,5 @@ def build_gateway_locust_grpc_client(environment: Environment) -> Channel:
     """
     locust_interceptor = LocustInterceptor(environment=environment)
 
-    channel = insecure_channel("localhost:9003")
+    channel = insecure_channel(settings.gateway_grpc_client.client_url)
     return intercept_channel(channel, locust_interceptor)
--- clients/http/gateway/client.py
+++ clients/http/gateway/client.py
@@ -2,6 +2,7 @@ from httpx import Client
 from locust.env import Environment
 from clients.http.event_hooks.locust_event_hook import locust_request_event_hook, locust_response_event_hook
 import logging
+from config import settings
 
 def build_gateway_http_client() -> Client:
     """
@@ -9,7 +10,10 @@ def build_gateway_http_client() -> Client:
 
     :return: Готовый к использованию объект httpx.Client.
     """
-    return Client(timeout=100, base_url="http://localhost:8003")
+    return Client(
+        timeout=settings.gateway_http_client.timeout,
+        base_url=settings.gateway_http_client.client_url
+    )
 
 
 def build_gateway_locust_http_client(environment: Environment) -> Client:
@@ -29,8 +33,8 @@ def build_gateway_locust_http_client(environment: Environment) -> Client:
     """
     logging.getLogger("httpx").setLevel(logging.WARNING)
     return Client(
-        timeout=100,
-        base_url="http://localhost:8003",
+        timeout=settings.gateway_http_client.timeout,
+        base_url=settings.gateway_http_client.client_url,
         event_hooks={
             "request": [locust_request_event_hook],
             "response": [locust_response_event_hook(environment)]
